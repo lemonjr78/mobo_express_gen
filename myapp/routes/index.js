d60d852860d50d35fbdafb5738224e5e49b3386e
lemonjr78 <lemonjr78@yahoo.com>
2015-10-28 17:39:57 +0700

    first push

---
+++ myapp/routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+
+// router.get('/', function(req,res){
+// 	// res.render('index' , {
+
+// 	// 	title : 'C++',
+// 	// 	name : 'ali'
+// 	// });
+// 	res.render('index' , req.query);
+// });
+router.get('/:title/:name', function(req,res){
+	res.render('index' , req.params);
+});
+module.exports = router;
